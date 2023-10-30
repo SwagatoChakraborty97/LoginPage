cda2bce239f7d449d1e181afd312dd84d31f0dc7
Swagato Chakraborty <swagatochakraborty123@gmail.com>
2023-10-30 20:05:49 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Login } from "./components/Login/Login";
+// import { Login } from "./components/Login/Login";
+import { Login } from "./components/Login/Login.styled";
 
 function App() {
   return (
     <>
-    <Login></Login>
+      {/* <Login></Login> */}
+      <Login/>
     </>
   );
 }
--- src/components/Login/Login.styled.jsx
+++ src/components/Login/Login.styled.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import styled from 'styled-components';
-import BannerImg from '../Login/banner_img.png';
+import React from "react";
+import styled from "styled-components";
+import BannerImg from "../Login/banner_img.png";
 
 const LoginContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100%;
+  @media (max-width: 1024px) {
+    display: block;
+  }
+  @media (max-width: 500px) {
+    font-size: small;
+  }
+  @media (max-width: 1500px) {
+    margin: 11% 0;
+  }
 `;
 
 const ImageContainer = styled.div`
@@ -16,16 +25,38 @@ const ImageContainer = styled.div`
 
 const BannerImage = styled.img`
   width: 90%;
+  @media (max-width: 1024px) {
+    width: 60%;
+    margin-top: 2%;
+  }
+  @media (max-width: 500px) {
+    margin-top: 6%;
+  }
 `;
 
 const Content = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding: 10% 6% 10%;
+  padding: 7% 5% 7%;
   border-radius: 8px;
   box-shadow: 0px 0px 7px 0px #0000004f;
   margin: 6%;
+  @media (max-width: 1500px) {
+    margin: 1%;
+  }
+  @media (max-width: 1200px) {
+    margin: 1%;
+    padding: 6% 3% 6%;
+  }
+  @media (max-width: 1024px) {
+    margin: 2%;
+    padding: 3% 1% 3%;
+  }
+
+  @media (max-width: 500px) {
+    margin-top: 6%;
+  }
 `;
 
 const Header = styled.h1`
@@ -33,36 +64,27 @@ const Header = styled.h1`
   align-self: center;
 `;
 
-const Error = styled.div`
-  background: coral;
-  border-radius: 5px;
-  padding: 2px 50px;
-  margin: 2px;
-  align-self: center;
-`;
-
 const Input = styled.input`
   padding: 4%;
   font-size: medium;
-`;
-
-const FormFields = styled.div`
-  min-width: 415%;
-`;
-
-const Item = styled.div`
-  margin: 2%;
+  min-width: 360%;
+  margin: 2% 0;
+  @media (max-width: 1024px) {
+    min-width: max-content;
+    padding: 1%;
+    min-width: 92vw;
+    margin: 0.2%;
+  }
 `;
 
 const Field = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 4px 500px 0px 0px;
-`;
-
-const Name = styled.div`
-  display: flex;
-  gap: 10px;
+  padding: 14px 500px 0px 0px;
+  @media (max-width: 1024px) {
+    padding: 0;
+    margin: 1.2% 0 0 0.5%;
+  }
 `;
 
 const Button = styled.a`
@@ -91,10 +113,16 @@ const CheckboxContainer = styled.div`
   display: flex;
   max-width: 98%;
   justify-content: space-between;
+  margin: 1% 0;
+  @media (max-width: 1024px) {
+    display: flex;
+    max-width: none;
+    justify-content: space-between;
+  }
 `;
 
 const Checkbox = styled.div`
-  padding: 2px 16px 2px;
+  padding: 2px 5px 2px;
 `;
 
 const CheckboxInput = styled.input`
@@ -105,16 +133,27 @@ const CheckboxInput = styled.input`
 const ChangePasswordLink = styled.a`
   color: #ff9138;
   text-decoration: none;
+
+  @media (max-width: 500px) {
+    text-align: center;
+  }
 `;
 
-const media = {
-  mobile: '@media (max-width: 500px)',
-  tablet: '@media (max-width: 1024px)',
-  laptop: '@media (max-width: 1200px)',
-  desktop: '@media (max-width: 1500px)',
-};
-
 export const Login = () => {
+  const togglePassword = () => {
+    const pswrd = document.getElementById("password_1");
+    const eyeicon = document.getElementById("eye_1");
+    // console.log(pswrd.type);
+    // console.log(eyeicon[0].textContent);
+    if (pswrd.type == "password") {
+      pswrd.type = "text";
+      eyeicon.textContent = "visibility";
+    } else {
+      pswrd.type = "password";
+      eyeicon.textContent = "visibility_off";
+    }
+    // console.log(pswrd.type, eyeicon.textContent);
+  };
   return (
     <>
       <LoginContainer>
@@ -123,7 +162,7 @@ export const Login = () => {
         </ImageContainer>
         <Content>
           <Header className="bold">Login</Header>
-          <div className="field item">
+          <Field>
             <label htmlFor="" className="item bold">
               Login Id
             </label>
@@ -134,8 +173,8 @@ export const Login = () => {
               placeholder="Login Id"
               className="form-fields"
             />
-          </div>
-          <div className="field item">
+          </Field>
+          <Field>
             <label htmlFor="" className="item bold">
               Password
             </label>
@@ -146,18 +185,19 @@ export const Login = () => {
               placeholder="Enter Password"
               className="form-fields"
             />
-          </div>
+          </Field>
           <CheckboxContainer>
             <div id="chkbox">
               <Checkbox>
                 <CheckboxInput type="checkbox" name="" id="" />
-                <label htmlFor="">&nbsp; Remember Me</label>
+                <label htmlFor=""> Remember Me</label>
               </Checkbox>
               <Checkbox>
                 <CheckboxInput type="checkbox" name="" id="" />
                 <label htmlFor="">
                   <span>
-                    Agree to{' '}
+                    {" "}
+                    Agree to{" "}
                     <SpanLink href="#" className="bold">
                       Terms and Conditions
                     </SpanLink>
@@ -171,7 +211,7 @@ export const Login = () => {
           </CheckboxContainer>
           <Button href="#">Login</Button>
           <Span>
-            Don't have an account?
+            Don't have an account?{" "}
             <SpanLink href="#" className="bold">
               Register Here
             </SpanLink>
